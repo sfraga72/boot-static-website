1667e03228805f891e4b3803cc25c01eca5f821d
sfraga72 <sfraga72@gmail.com>
2025-11-21 16:02:01 +1300

    fixes to L1

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
-from textnode import TextNode
+from textnode import TextNode, TextType
 
-print("hello world")
 
 def main():
-    new_node = TextNode("This is some anchor text", "plain", "https://www.boot.dev")
-    print(new_node)
+    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(node)
+
 
 main()
