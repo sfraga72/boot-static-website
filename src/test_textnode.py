da1215e208157512691f67828fdc9670acf01616
sfraga72 <sfraga72@gmail.com>
2025-11-21 16:10:28 +1300

    L2: TextNode Tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,36 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_eq_with_url(self):
+        node = TextNode("This is a link", TextType.LINK, "http://example.com")
+        node2 = TextNode("This is a link", TextType.LINK, "http://example.com")
+        self.assertEqual(node, node2)
+    
+    def test_neq_different_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is another text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+    
+    def test_neq_different_type(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+    
+    def test_neq_different_url(self):
+        node = TextNode("This is a link", TextType.LINK, "http://example.com")
+        node2 = TextNode("This is a link", TextType.LINK, "http://different.com")
+        self.assertNotEqual(node, node2)
+
+    def test_neq_one_without_url(self):
+        node = TextNode("This is a link", TextType.LINK, "http://example.com")
+        node2 = TextNode("This is a link", TextType.LINK)
+        self.assertNotEqual(node, node2)
+    
+    def test_repr(self):
+        node = TextNode("Sample text", TextType.CODE, "http://example.com")
+        expected_repr = "TextNode(Sample text, code, http://example.com)"
+        self.assertEqual(repr(node), expected_repr)
+
 
 if __name__ == "__main__":
     unittest.main()
